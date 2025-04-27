853d3a045fb2b5feb63fcbea2997f591846f3297
Juanan Pereira <juanan.pereira@ehu.eus>
2025-04-27 12:39:44 +0200

    Enhance JavaFX module configuration and update HelloController to handle ActionEvent in button click method.

--- src/main/java/eus/ehu/i18n/HelloController.java
+++ src/main/java/eus/ehu/i18n/HelloController.java
@@ -1,5 +1,6 @@
 package eus.ehu.i18n;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import java.util.ResourceBundle;
@@ -17,7 +18,7 @@ public class HelloController {
     }
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onHelloButtonClick(ActionEvent event) {
         welcomeText.setText(bundle.getString("clicked"));
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module eus.ehu.i18n {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
 
 
     opens eus.ehu.i18n to javafx.fxml;
